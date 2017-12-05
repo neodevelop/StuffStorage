ce00fc36312d8a55e26c3a209525033700424a29
neodevelop <juan@makingdevs.com>
2017-12-05 12:41:13 -0600

    Ajustando imports y quitando código no necesario

--- app/src/main/java/com/makingdevs/stuffstorage/FruitManager.java
+++ app/src/main/java/com/makingdevs/stuffstorage/FruitManager.java
@@ -1,18 +1,7 @@
 package com.makingdevs.stuffstorage;
 
-import android.os.AsyncTask;
-
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-
-import retrofit2.Call;
-import retrofit2.Response;
-import retrofit2.Retrofit;
-import retrofit2.converter.gson.GsonConverterFactory;
 
 /**
  * Created by makingdevs on 23/11/17.
@@ -22,68 +11,32 @@ public class FruitManager {
 
     private static FruitManager fruitManager = null;
 
-    private static List<Fruit> mFruits = null;
-
-    private static FruitService fruitService;
+    private static List<Fruit> fruits = null;
 
     private FruitManager(){
-        this.mFruits = new ArrayList<Fruit>();
+        this.fruits = new ArrayList<Fruit>();
     }
 
     public static FruitManager getInstance(){
-        if(fruitService == null){
-            Gson gson = new GsonBuilder()
-                    .setDateFormat("yyyy-MM-dd'T'HH:mm:ssZ")
-                    .create();
-            Retrofit retrofit = new Retrofit.Builder()
-                    .baseUrl("http://192.168.1.134:8080/v1/")
-                    .addConverterFactory(GsonConverterFactory.create(gson))
-                    .build();
-           fruitService = retrofit.create(FruitService.class);
-        }
         if(fruitManager == null)
             fruitManager = new FruitManager();
         return fruitManager;
     }
 
     public int size(){
-        return this.mFruits.size();
+        return this.fruits.size();
     }
 
     public void addFruit(Fruit fruit){
-        this.mFruits.add(fruit);
+        this.fruits.add(fruit);
     }
 
     public Fruit getFruit(int index){
-        return this.mFruits.get(index);
+        return this.fruits.get(index);
     }
 
     public List<Fruit> getFruits(){
-        Call<List<Fruit>> call = fruitService.listFruits();
-        new RemoteListFruits().execute(call);
-        return mFruits;
+        return fruits;
     }
 
-    private class RemoteListFruits extends AsyncTask<Call, Void, List<Fruit>> {
-        @Override
-        protected List<Fruit> doInBackground(Call... calls) {
-            try {
-                Call<List<Fruit>> call = calls[0];
-                Response<List<Fruit>> response = call.execute();
-                List<Fruit> fruits = response.body();
-                System.out.println(fruits);
-                mFruits = fruits;
-                return fruits;
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            return null;
-
-        }
-
-        @Override
-        protected void onPostExecute(List<Fruit> fruits) {
-            mFruits = fruits;
-        }
-    }
 }
--- app/src/main/java/com/makingdevs/stuffstorage/NewFruitActivity.java
+++ app/src/main/java/com/makingdevs/stuffstorage/NewFruitActivity.java
@@ -1,16 +1,12 @@
 package com.makingdevs.stuffstorage;
 
-import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
-import android.os.Parcelable;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-
-import java.io.Serializable;
 
 public class NewFruitActivity extends AppCompatActivity {
 
